c7f958d3150961bd51115b84af32fcf08b0314e6
tarielTitirashvili <tarieltitirashvili@gmail.com>
2025-05-25 12:19:15 +0400

    robHouses with constant memory

--- robHouse.js
+++ robHouse.js
@@ -14,3 +14,20 @@ console.log(smartRobbersPath([1, 2, 3, 1]))
 console.log(smartRobbersPath([5, 2, 4, 6]))
 console.log(smartRobbersPath([2, 7, 9, 3, 1, 6]))
 console.log(smartRobbersPath([4, 11, 10, 2, 1, 8, 5]))
+
+function smartRobbersPathMemoryOptimized(houses) {
+  if (houses.length === 0) return 0
+  if (houses.length === 1) return houses[0]
+  let [pointer0, pointer1, pointer2] = [houses[0], Math.max(houses[0], houses[1])]
+  for (let i = 2;i < houses.length;i++) {
+    pointer2 = Math.max(pointer1, houses[i] + pointer0)
+    pointer0 = pointer1
+    pointer1 = pointer2
+  }
+  return maxPointers[maxPointers.length-1]
+}
+
+console.log(smartRobbersPath([1, 2, 3, 1]))
+console.log(smartRobbersPath([5, 2, 4, 6]))
+console.log(smartRobbersPath([2, 7, 9, 3, 1, 6]))
+console.log(smartRobbersPath([4, 11, 10, 2, 1, 8, 5]))
